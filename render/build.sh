47b474195f49414821f96e65c499d7490cb57ca9
Jose Olguin Lagos <hellocodefusion@gmail.com>
2025-10-29 21:38:33 -0300

    deploy_test

---
+++ render/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# exit on error
+set -o errexit
+
+pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+# python build.py
